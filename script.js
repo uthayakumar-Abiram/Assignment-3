248976e30aadd02064b66d2fdaa7022009391772
Abiram <uthayakumarabiram@gmail.com>
2023-11-13 22:04:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+ function myFunction() {
+
+    document.body.classList.toggle("darkmode");
+ }
+ // Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .card");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector("#filter-buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return card.classList.replace("hide", "show");
+        }
+        card.classList.add("hide");
+    });
+}
+//zoom in
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+function zoomin() { 
+   var currWidth = document.getElementById("geeks").clientWidth; 
+   document.getElementById("geeks").style.width = (currWidth + 100) + "px"; 
+} 
+ 
+function zoomout() {  
+   document.getElementById("geeks").style.width = (600) + "px"; 
+} 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
